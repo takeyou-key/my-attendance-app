f106e13bce77006f1bc9ddcb6c97207392a1b57c
たきざわ <yuk.takizawa@gmail.com>
2025-08-29 12:57:51 +0900

    レイアウト調整

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -90,7 +90,7 @@ const Settings = () => {
   return (
     <AuthGuard onAuthStateChange={handleAuthStateChange}>
       <div className="h-full">
-        <div className="max-w-4xl mx-auto p-3 md:p-6 pb-20 lg:pb-6">
+        <div className="max-w-4xl mx-auto p-4 md:p-6 pb-20 lg:pb-6">
           <div className="bg-white rounded-lg shadow-md p-4 md:p-6">
             
             <div className="space-y-4 md:space-y-6">
